45514fbcf23e189f7043a0134c0d554418d2732f
Rebecca <rxc1056@miami.edu>
2025-08-10 18:04:44 -0400

    feat(static): change to vercel static from vercel serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel/serverless'; // v7 uses serverless adapter
+import vercel from '@astrojs/vercel/static'; 
 
 export default defineConfig({
   site: 'https://rebeccacamejo.com', // Replace with your domain
-  output: 'server',
+  output: 'static',
   integrations: [
     tailwind(),
     sitemap(),
