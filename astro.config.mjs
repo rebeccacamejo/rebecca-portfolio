46855f2c580f31cdb83389b459ec82c3e72991f9
Rebecca <rcamejo04@gmail.com>
2025-08-11 21:57:49 -0400

    chore(fix): fix vercel and dependency conflicts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel/serverless'; 
+import vercel from '@astrojs/vercel'; 
 
 export default defineConfig({
-  site: 'https://rebeccacamejo.com', // Replace with your domain
+  site: 'https://rebeccacamejo.com', 
   output: 'server',
   integrations: [
     tailwind(),
